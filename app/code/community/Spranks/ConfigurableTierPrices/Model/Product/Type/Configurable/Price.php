96022788571f10727e591f6d434c53dfd5d76497
Simon Sprankel <simonsprankel@googlemail.com>
2012-03-12 14:34:55 +0100

    Fixed some formatting problems

--- app/code/community/Spranks/ConfigurableTierPrices/Model/Product/Type/Configurable/Price.php
+++ app/code/community/Spranks/ConfigurableTierPrices/Model/Product/Type/Configurable/Price.php
@@ -47,19 +47,19 @@ class Spranks_ConfigurableTierPrices_Model_Product_Type_Configurable_Price exten
         // example: if a shirt is available in red and black and if you buy 
         // three or more the price is eight euro, you can also buy one red and 
         // two black shirts and you will get the tier price of eight euro.
-		// based on https://www.magentocommerce.com/boards/viewthread/10743/
-		if ($product->getTierPriceCount() > 0) {
-			$tierPrice = $this->calcConfigProductTierPricing($product);
-			if ($tierPrice < $finalPrice) {
-				$finalPrice = $tierPrice;
-			}
-		}
-		return $finalPrice;
+        // based on https://www.magentocommerce.com/boards/viewthread/10743/
+        if ($product->getTierPriceCount() > 0) {
+            $tierPrice = $this->calcConfigProductTierPricing($product);
+            if ($tierPrice < $finalPrice) {
+                $finalPrice = $tierPrice;
+            }
+        }
+        return $finalPrice;
     }
 
-	/**
+    /**
      * Get product final price via configurable product's tier pricing structure. 
-	 * Uses qty of parent item to determine price.
+     * Uses qty of parent item to determine price.
      *
      * @param   Mage_Catalog_Model_Product $product
      * @return  float
@@ -68,29 +68,29 @@ class Spranks_ConfigurableTierPrices_Model_Product_Type_Configurable_Price exten
     {
         $tierPrice = PHP_INT_MAX;
 
-		if ($items = Mage::getSingleton('checkout/session')->getQuote()->getItemsCollection()) {
-			// map mapping the IDs of the parent products with the quantities of the corresponding simple products
-			$idQuantities = array();
-			// go through all products in the quote
-			foreach ($items as $item) {
-				if ($item->getParentItem())
-					continue;
-				// get the simple products ID
-				$productModel = Mage::getModel('catalog/product');
-				$id = $productModel->getIdBySku($item->getSku());
-				// get the parent IDs
-				$configurableProductModel = Mage::getModel('catalog/product_type_configurable');
-				$parentIdArray = $configurableProductModel->getParentIdsByChild($id);
-				// map the parent ID with the quantities of the simple products
-				foreach ($parentIdArray as $parent)
-					$idQuantities[$parent][] = $item->getQty();
-			}
-			// compute the total quantity of items of the configurable product
-			if (array_key_exists($product->getId(), $idQuantities)) {
-				$totalQty = array_sum($idQuantities[$product->getId()]);
-				$tierPrice = parent::getFinalPrice($totalQty, $product);
-			}
-		}
+        if ($items = Mage::getSingleton('checkout/session')->getQuote()->getItemsCollection()) {
+            // map mapping the IDs of the parent products with the quantities of the corresponding simple products
+            $idQuantities = array();
+            // go through all products in the quote
+            foreach ($items as $item) {
+                if ($item->getParentItem())
+                    continue;
+                // get the simple products ID
+                $productModel = Mage::getModel('catalog/product');
+                $id = $productModel->getIdBySku($item->getSku());
+                // get the parent IDs
+                $configurableProductModel = Mage::getModel('catalog/product_type_configurable');
+                $parentIdArray = $configurableProductModel->getParentIdsByChild($id);
+                // map the parent ID with the quantities of the simple products
+                foreach ($parentIdArray as $parent)
+                    $idQuantities[$parent][] = $item->getQty();
+            }
+            // compute the total quantity of items of the configurable product
+            if (array_key_exists($product->getId(), $idQuantities)) {
+                $totalQty = array_sum($idQuantities[$product->getId()]);
+                $tierPrice = parent::getFinalPrice($totalQty, $product);
+            }
+        }
         return $tierPrice;
     }
 
