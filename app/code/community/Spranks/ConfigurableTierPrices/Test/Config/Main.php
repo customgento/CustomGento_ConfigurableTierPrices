d0698456ebfab9ad71f565b1dcdf491b5254d5a6
Simon Sprankel <simonsprankel@gmail.com>
2014-11-18 15:51:36 +0100

    formatting, add messages to tests

--- app/code/community/Spranks/ConfigurableTierPrices/Test/Config/Main.php
+++ app/code/community/Spranks/ConfigurableTierPrices/Test/Config/Main.php
@@ -3,29 +3,36 @@
 class Spranks_ConfigurableTierPrices_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config
 {
 
-	/**
-	 * Check it the installed module has the correct module version
-	 */
-	public function testModuleConfig()
-	{
-		$this->assertModuleVersionGreaterThanOrEquals($this->expected('module')->getVersion());
-		$this->assertModuleCodePool($this->expected('module')->getCodePool());
-	}
+    /**
+     * Check it the installed module has the correct module version
+     */
+    public function testModuleConfig()
+    {
+        $this->assertModuleVersionGreaterThanOrEquals($this->expected('module')->getVersion(), 'module is new enough');
+        $this->assertModuleCodePool($this->expected('module')->getCodePool(), 'correct module code pool');
+        $this->assertModuleIsActive('module is active');
+    }
 
-	/**
-	 * Check if the helper aliases are returning the correct class names
-	 */
-	public function testHelperAliases()
-	{
-		$this->assertHelperAlias('spranks_configurabletierprices/admin', 'Spranks_ConfigurableTierPrices_Helper_Admin');
-	}
+    /**
+     * Check if the helper aliases are returning the correct class names
+     */
+    public function testHelperAliases()
+    {
+        $this->assertHelperAlias(
+            'spranks_configurabletierprices/admin', 'Spranks_ConfigurableTierPrices_Helper_Admin',
+            'correct helper alias'
+        );
+    }
 
-	/**
-	 * Check if the helper aliases are returning the correct class names
-	 */
-	public function testModelAliases()
-	{
-		$this->assertModelAlias('spranks_configurabletierprices/observer', 'Spranks_ConfigurableTierPrices_Model_Observer');
-	}
+    /**
+     * Check if the helper aliases are returning the correct class names
+     */
+    public function testModelAliases()
+    {
+        $this->assertModelAlias(
+            'spranks_configurabletierprices/observer', 'Spranks_ConfigurableTierPrices_Model_Observer',
+            'correct model alias'
+        );
+    }
 
 }
